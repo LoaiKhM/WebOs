8530dda4bd1702803248285b7d0177ed013e62b3
Loai Khalid <148443990+python3dev@users.noreply.github.com>
2024-08-29 17:28:12 +0000

    rtf

--- public/applications/terminal/application.js
+++ public/applications/terminal/application.js
@@ -1,21 +1,8 @@
 // api : cmd [0] is the tool // cmd [1] is the parameters
-function terminal_system(req,res){
+async function terminal_system(req,res){
     let serv = req.body.serv;
     if(serv == 'terminal-req'){
-        let cmd = req.body.cmd;
-        // make the list that contains the tool the first space and the rest is in one item in the list !
-        cmd = cmd.split(' ')
-        let first = cmd.shift()
-        cmd = cmd.join(' ')
-        // the finsher list 
-        let toolparm = [first, cmd]
-        // the requirement of the tool 
-        let reqs = require(`./public/applications/terminal/terminal-tools/${toolparm[0]}.jst`)
-        // the passing of the parameters!
-        let out = reqs(toolparm[1])
-        // sending the respond of the server and the tool ..
-        res.send({error : false, output : out})
-
+        console.log(req.body.nwJSN)
     }
 }
 // export it as a module
